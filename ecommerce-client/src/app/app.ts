650cf148c99357279efa3d95a5701170881ae47b
mubasherjam <mubasherworld@gmail.com>
2026-09-23 17:29:18 +0500

    finalize Shopfront ecommerce application

--- ecommerce-client/src/app/app.ts
+++ ecommerce-client/src/app/app.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from './shared/components/navbar/navbar';
 
+
 @Component({
   selector: 'app-root',
   standalone: true,
